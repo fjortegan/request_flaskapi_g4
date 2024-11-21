00e59ca2022113c3598a03fc1b9765ff7263cfe8
alejandro-diaz-barea <126179563+alejandro-diaz-barea@users.noreply.github.com>
2024-11-21 13:51:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,  Request
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -27,6 +27,62 @@ async def handle_post(request: Request):
     url = str(request.url)
     body = await request.body()
     
+    return {
+        "method": method,
+        "url": url,
+        "headers": headers,
+        "body": body.decode('utf-8')
+    }
+
+@app.options("/")
+async def handle_options(request: Request):
+    headers = dict(request.headers)
+    method = request.method
+    url = str(request.url)
+    body = await request.body()
+
+    return {
+        "method": method,
+        "url": url,
+        "headers": headers,
+        "body": body.decode('utf-8')
+    }
+
+@app.put("/")
+async def handle_put(request: Request):
+    headers = dict(request.headers)
+    method = request.method
+    url = str(request.url)
+    body = await request.body()
+
+    return {
+        "method": method,
+        "url": url,
+        "headers": headers,
+        "body": body.decode('utf-8')
+    }
+
+@app.patch("/")
+async def handle_patch(request: Request):
+    headers = dict(request.headers)
+    method = request.method
+    url = str(request.url)
+    body = await request.body()
+
+    return {
+        "method": method,
+        "url": url,
+        "headers": headers,
+        "body": body.decode('utf-8')
+    }
+
+@app.delete("/")
+async def handle_delete(request: Request):
+    headers = dict(request.headers)
+    method = request.method
+    url = str(request.url)
+    body = await request.body()
+
     return {
         "method": method,
         "url": url,
