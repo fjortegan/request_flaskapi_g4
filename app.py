c85c4832197b739e7c63ab0faf7ff64155c240d2
alejandro-diaz-barea <126179563+alejandro-diaz-barea@users.noreply.github.com>
2024-11-21 13:56:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
+from typing import List, Union
 
 app = FastAPI()
 
+# Datos en memoria
+data: Union[List[str], List[int]] = []
+
 @app.get("/")
-async def handle_get(request: Request):
+async def get_data(request: Request):
     headers = dict(request.headers)
     query_params = dict(request.query_params)
     method = request.method
@@ -17,75 +21,96 @@ async def handle_get(request: Request):
         "headers": headers,
         "query_params": query_params,
         "cookies": cookies,
-        "body": body.decode('utf-8')
+        "body": body.decode('utf-8'),
+        "data": data
     }
 
 @app.post("/")
-async def handle_post(request: Request):
+async def add_data(request: Request, item: Union[str, int]):
     headers = dict(request.headers)
+    query_params = dict(request.query_params)
     method = request.method
     url = str(request.url)
+    cookies = request.cookies
     body = await request.body()
     
+    data.append(item)
     return {
         "method": method,
         "url": url,
         "headers": headers,
-        "body": body.decode('utf-8')
-    }
-
-@app.options("/")
-async def handle_options(request: Request):
-    headers = dict(request.headers)
-    method = request.method
-    url = str(request.url)
-    body = await request.body()
-
-    return {
-        "method": method,
-        "url": url,
-        "headers": headers,
-        "body": body.decode('utf-8')
+        "query_params": query_params,
+        "cookies": cookies,
+        "body": body.decode('utf-8'),
+        "message": "Item added",
+        "data": data
     }
 
 @app.put("/")
-async def handle_put(request: Request):
+async def replace_data(request: Request, new_data: List[Union[str, int]]):
     headers = dict(request.headers)
+    query_params = dict(request.query_params)
     method = request.method
     url = str(request.url)
+    cookies = request.cookies
     body = await request.body()
-
+    
+    global data
+    data = new_data
     return {
         "method": method,
         "url": url,
         "headers": headers,
-        "body": body.decode('utf-8')
+        "query_params": query_params,
+        "cookies": cookies,
+        "body": body.decode('utf-8'),
+        "message": "Data replaced",
+        "data": data
     }
 
 @app.patch("/")
-async def handle_patch(request: Request):
+async def update_data(request: Request, index: int, new_value: Union[str, int]):
     headers = dict(request.headers)
+    query_params = dict(request.query_params)
     method = request.method
     url = str(request.url)
+    cookies = request.cookies
     body = await request.body()
-
+    
+    if index < 0 or index >= len(data):
+        raise HTTPException(status_code=400, detail="Index out of range")
+    data[index] = new_value
     return {
         "method": method,
         "url": url,
         "headers": headers,
-        "body": body.decode('utf-8')
+        "query_params": query_params,
+        "cookies": cookies,
+        "body": body.decode('utf-8'),
+        "message": "Item updated",
+        "data": data
     }
 
 @app.delete("/")
-async def handle_delete(request: Request):
+async def delete_data(request: Request, index: int):
     headers = dict(request.headers)
+    query_params = dict(request.query_params)
     method = request.method
     url = str(request.url)
+    cookies = request.cookies
     body = await request.body()
-
+    
+    if index < 0 or index >= len(data):
+        raise HTTPException(status_code=400, detail="Index out of range")
+    removed_item = data.pop(index)
     return {
         "method": method,
         "url": url,
         "headers": headers,
-        "body": body.decode('utf-8')
+        "query_params": query_params,
+        "cookies": cookies,
+        "body": body.decode('utf-8'),
+        "message": "Item deleted",
+        "removed_item": removed_item,
+        "data": data
     }
