76357bdf4ec919641f859a4e8be70cfe73e93c3d
alejandro-diaz-barea <126179563+alejandro-diaz-barea@users.noreply.github.com>
2024-12-01 14:18:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = FastAPI(debug=True)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://wonderful-druid-9bfaf6.netlify.app"],  # Cambia esto si tu frontend corre en otra URL
+    allow_origins=["*"],  # Cambia esto si tu frontend corre en otra URL
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type"],
